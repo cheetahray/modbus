ec11b74f7585d30b0dc4b22f7972605d71a08236
cheetahray <ray608@gmail.com>
2018-06-07 14:06:13 +0800

    for Single Motor Controller

---
+++ debug.py
@@ -0,0 +1,58 @@
+from ModbusClient import *
+import time
+import OSC
+import _thread
+import sys
+import types
+import math
+import random
+import argparse
+
+modbusClient = ModbusClient('COM7') #modbusClient = ModbusClient('127.0.0.1', 502)
+#modbusClient.Parity = Parity.odd
+modbusClient.Parity = Parity.even
+modbusClient.UnitIdentifier = 1
+modbusClient.Baudrate = 115200
+modbusClient.Stopbits = Stopbits.one
+#modbusClient.timeout = 0.001
+modbusClient.Connect()
+
+# incremental absolute
+#print modbusClient.ReadHoldingRegisters(0x0336, 1, 14)
+
+# Clear zero
+#modbusClient.WriteSingleRegister(0x0338, 1, 14)    
+#print modbusClient.ReadHoldingRegisters(0x0338, 1, 14)
+
+# Clear Alert
+#print modbusClient.ReadHoldingRegisters(0x0524, 1, 15)
+#modbusClient.WriteSingleRegister(0x0524, 1, 15)    
+
+# Read Alert
+#print modbusClient.ReadHoldingRegisters(0x0100, 1, 14)
+
+# Read Distance
+#holdingRegisters = modbusClient.ReadHoldingRegisters(0x0024, 2, 11) #holdingRegisters = ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(2304, 1))
+#print holdingRegisters
+#print ( (holdingRegisters[1] << 16) + holdingRegisters[0] )
+
+# incremental absolute
+#modbusClient.WriteSingleRegister(0x0300, 4096, 14)    
+#print modbusClient.ReadHoldingRegisters(0x0300, 1, 14)
+
+# baudrate
+#modbusClient.WriteSingleRegister(0x052A, 87, 14)    
+#print modbusClient.ReadHoldingRegisters(0x052A, 1, 14)
+
+# Clear Zero not Move
+#modbusClient.WriteSingleRegister(0x0306, 33, 14)    
+#print modbusClient.ReadHoldingRegisters(0x0306, 1, 14)
+
+# Go Zero
+modbusClient.WriteSingleRegister(0x08A2, 1000, 11)
+modbusClient.WriteSingleRegister(0x08A2, 1000, 10)
+# Write PR
+#motordistance = 3937800000
+#idx = 1
+#modbusClient.WriteMultipleRegisters(0x0838, [0x0012 + (idx << 8) + (idx << 12), idx, motordistance & 0xFFFF, motordistance >> 16], 14)
+#modbusClient.WriteSingleRegister(0x08A2, 63, 14)
